eb511b47322cfb9a450ca90d479146c089d49a65
devkulkarni <kulkarni.devdatta@gmail.com>
2016-06-22 11:50:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from jinja2 import Environment, FileSystemLoader
 ENV = Environment(loader=FileSystemLoader('templates'))
 
 
-def test_method_56():
+def test_method_57():
     print("This is a test method")
 
 
